a5f8c348829f179f0d96619367d7f2ed9a79f2ea
Gpeter28 <995614092@qq.com>
2021-08-29 17:45:17 +0800

    Add New field Rating

--- RazorPagesMoviesDemo/Migrations/20210829093943_Rating.Designer.cs
+++ RazorPagesMoviesDemo/Migrations/20210829093943_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMoviesDemo.Data;
 
 namespace RazorPagesMoviesDemo.Migrations
 {
     [DbContext(typeof(RazorPagesMoviesDemoContext))]
-    partial class RazorPagesMoviesDemoContextModelSnapshot : ModelSnapshot
+    [Migration("20210829093943_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
